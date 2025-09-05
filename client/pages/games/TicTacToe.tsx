1ceba816c3c36869d69f7122e6f6602e30f3167c
rajshekar1206 <b.rajshekar1212@gmail.com>
2025-09-05 10:22:50 +0530

    Add client/pages/games/TicTacToe.tsx

---
+++ client/pages/games/TicTacToe.tsx
@@ -0,0 +1,25 @@
+import Layout from "@/components/Layout";
+import TicTacToeGame from "@/components/games/TicTacToeGame";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
+
+export default function TicTacToePage() {
+  return (
+    <Layout>
+      <div className="container mx-auto px-4 py-8">
+        {/* Breadcrumb */}
+        <div className="mb-6">
+          <Button asChild variant="ghost" size="sm">
+            <Link to="/games">
+              <ArrowLeft className="h-4 w-4 mr-2" />
+              Back to Games
+            </Link>
+          </Button>
+        </div>
+
+        <TicTacToeGame />
+      </div>
+    </Layout>
+  );
+}
